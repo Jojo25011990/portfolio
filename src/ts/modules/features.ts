a1cea27c7e6656fd7bde96999256f67a8d36d0f2
Jojo25011990 <Jojo25011990@gmail.com>
2026-04-02 21:17:30 +0200

    add all

--- src/ts/modules/features.ts
+++ src/ts/modules/features.ts
@@ -9,6 +9,17 @@ export default function features() {
     '.features__description',
   );
 
+  const featuresItems =
+    document.querySelectorAll<HTMLLIElement>('.features__item');
+
+  const featuresItemsIcon = document.querySelectorAll<HTMLElement>(
+    '.features__item-icon',
+  );
+  const newFeaturesItemsIcon = Array.from(featuresItemsIcon);
+
+  const randomSpeed = 2000;
+  let previousItemIndex: number | null = null;
+
   if (featuresDescription) {
     gsap.to(featuresDescription, {
       scrollTrigger: {
@@ -21,4 +32,41 @@ export default function features() {
       ease: 'none',
     });
   }
+
+  if (featuresItems) {
+    featuresItems.forEach((featuresItem, index: number) => {
+      gsap.from(featuresItem, {
+        delay: index * 0.15,
+        y: 100,
+        autoAlpha: 0,
+        duration: 0.6,
+        ease: 'back.out',
+
+        scrollTrigger: {
+          trigger: featuresItem,
+          start: 'top 80%',
+          toggleActions: 'play none none none',
+        },
+      });
+    });
+  }
+
+  setInterval(() => {
+    let randomItemIndex: number;
+
+    do {
+      randomItemIndex = Math.trunc(Math.random() * featuresItems.length);
+    } while (randomItemIndex === previousItemIndex);
+    {
+      newFeaturesItemsIcon.forEach((featuresItemIcon, index: number) => {
+        if (randomItemIndex === index) {
+          featuresItemIcon.classList.add('random-active');
+        } else {
+          featuresItemIcon.classList.remove('random-active');
+        }
+      });
+
+      previousItemIndex = randomItemIndex;
+    }
+  }, randomSpeed);
 }
--- src/ts/modules/services.ts
+++ src/ts/modules/services.ts
@@ -9,6 +9,9 @@ export default function services() {
     '.services__description',
   );
 
+  const servicesItems =
+    document.querySelectorAll<HTMLLIElement>('.services__item');
+
   if (servicesDescription) {
     gsap.to(servicesDescription, {
       scrollTrigger: {
@@ -19,6 +22,21 @@ export default function services() {
       duration: 2,
       text: 'Exploring, building, and animating digital experiences that tell a story.',
       ease: 'none',
+    });
+  }
+
+  if (servicesItems.length) {
+    gsap.from(servicesItems, {
+      scrollTrigger: {
+        trigger: servicesItems[0],
+        start: 'top center',
+      },
+
+      stagger: 0.3,
+      autoAlpha: 0,
+      y: 100,
+      duration: 1.5,
+      ease: 'power2.out',
     });
   }
 }
