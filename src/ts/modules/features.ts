de112f945bf6855fc970aaa2be6990e58109a899
Jojo25011990 <Jojo25011990@gmail.com>
2026-04-04 03:47:59 +0200

    add all

--- src/ts/modules/features.ts
+++ src/ts/modules/features.ts
@@ -9,6 +9,8 @@ export default function features() {
     '.features__description',
   );
 
+  const featuresMenu =
+    document.querySelector<HTMLUListElement>('.features__menu');
   const featuresItems =
     document.querySelectorAll<HTMLLIElement>('.features__item');
 
@@ -20,6 +22,14 @@ export default function features() {
   const randomSpeed = 2000;
   let previousItemIndex: number | null = null;
 
+  const featuresFunFacts = document.querySelector<HTMLElement>(
+    '.features__fun-facts',
+  );
+  const featuresFunFactsButton = document.querySelector<HTMLButtonElement>(
+    '.features__fun-facts-button',
+  );
+
+  // *** Main Description - Paragraph Tag ***
   if (featuresDescription) {
     gsap.to(featuresDescription, {
       scrollTrigger: {
@@ -32,7 +42,9 @@ export default function features() {
       ease: 'none',
     });
   }
+  // *** End of Main Description - Paragraph Tag ***
 
+  // *** Features Eight Mini blocks ***
   if (featuresItems) {
     featuresItems.forEach((featuresItem, index: number) => {
       gsap.from(featuresItem, {
@@ -40,7 +52,7 @@ export default function features() {
         y: 100,
         autoAlpha: 0,
         duration: 0.6,
-        ease: 'back.out',
+        ease: 'power2.out',
 
         scrollTrigger: {
           trigger: featuresItem,
@@ -50,6 +62,24 @@ export default function features() {
       });
     });
   }
+
+  // *** Version 02 ***
+  //   if (featuresItems.length) {
+  //     gsap.from(featuresItems, {
+  //       scrollTrigger: {
+  //         trigger: featuresItems[0],
+  //         start: 'top 80%',
+  //         toggleActions: 'play none none none',
+  //       },
+
+  //       stagger: 0.3,
+  //       autoAlpha: 0,
+  //       y: 100,
+  //       duration: 1.5,
+  //       ease: 'power2.out',
+  //     });
+  //   }
+  // *** End of Version 02 ***
 
   setInterval(() => {
     let randomItemIndex: number;
@@ -69,4 +99,14 @@ export default function features() {
       previousItemIndex = randomItemIndex;
     }
   }, randomSpeed);
+  // *** End of Features Eight Mini blocks ***
+
+  // *** Fun Facts ***
+  const funFactsSlideBox = () => featuresFunFacts?.classList.toggle('active');
+  const funFactsContainer = () => featuresMenu?.classList.toggle('active');
+
+  featuresFunFactsButton?.addEventListener('click', funFactsSlideBox);
+  featuresFunFactsButton?.addEventListener('click', funFactsContainer);
+
+  // *** End of Fun Facts ***
 }
--- src/ts/modules/myStory.ts
+++ src/ts/modules/myStory.ts
@@ -5,4 +5,143 @@ gsap.registerPlugin(ScrollTrigger);
 
 export default function myStory() {
   console.log('Dragon');
+
+  const myStory = document.querySelector<HTMLElement>('.my-story');
+
+  const myStoryContainer = document.querySelector<HTMLDivElement>(
+    '.my-story__container',
+  );
+
+  const myStoryDragon =
+    document.querySelector<HTMLDivElement>('.my-story__dragon');
+
+  const myStoryWoman = document.querySelector<HTMLDivElement>(
+    '.my-story__character-img-woman',
+  );
+  const myStoryMan = document.querySelector<HTMLImageElement>(
+    '.my-story__character-img-man',
+  );
+
+  const myStoryWomanBubbles = document.querySelectorAll<HTMLDivElement>(
+    '.my-story__character-bubble-woman',
+  );
+  const myStoryManBubbles = document.querySelectorAll<HTMLDivElement>(
+    '.my-story__character-bubble-man',
+  );
+
+  const [myStoryManBubbles01, myStoryManBubbles02] = myStoryManBubbles;
+  const [myStoryWomanBubbles01, myStoryWomanBubbles02] = myStoryWomanBubbles;
+
+  console.log(myStoryWomanBubbles);
+
+  const gsapDragon = gsap.timeline();
+  const gsapBeforeDragon = gsap.timeline();
+  const gsapAfterDragon = gsap.timeline();
+
+  gsapDragon
+    .to(myStory, {
+      scrollTrigger: {
+        trigger: myStory,
+        start: 'top 10%',
+        toggleActions: 'play none none none',
+        markers: true,
+      },
+
+      //   duration: 1,
+      //   ease: 'power3',
+      //   repeat: 2,
+
+      //   keyframes: [
+      //     { x: '-7px' },
+      //     { x: '7px' },
+      //     { x: '-7px' },
+      //     { x: '7px' },
+      //     { x: '-7px' },
+      //   ],
+
+      //   onStart: () => {
+
+      //     gsapBeforeDragon.add(() => {
+      //       myStory?.classList.add('active');
+      //       console.log('ide to');
+      //     }, '-=3');
+      //   },
+
+      //   duration: 0.1,
+      //   repeat: 5,
+      //   yoyo: true,
+      //   x: 7,
+
+      //   onComplete: () => {
+      //     myStoryDragon?.classList.add('active');
+
+      //     myStoryDragon?.addEventListener('animationstart', () => {
+      //       gsapBeforeDragon
+      //         .add(() => {
+      //           myStoryManBubbles01.classList.add('active');
+      //           myStoryMan?.classList.add('active');
+      //         }, '+=.8')
+      //         .add(() => {
+      //           myStoryManBubbles01.classList.remove('active');
+      //           myStoryMan?.classList.remove('active');
+      //         }, '+=1.2')
+      //         .add(() => {
+      //           myStoryWomanBubbles01.classList.add('active');
+      //           myStoryWoman?.classList.add('active');
+      //         })
+      //         .add(() => {
+      //           myStoryWomanBubbles01.classList.remove('active');
+      //           myStoryWoman?.classList.remove('active');
+      //         }, '+=1.2');
+      //     });
+      //   },
+    })
+    .add(() => {
+      myStoryContainer?.classList.add('active');
+    })
+    .add(() => {
+      myStoryDragon?.classList.add('active');
+      myStoryDragon?.addEventListener('animationstart', () => {
+        gsapBeforeDragon
+          .add(() => {
+            myStoryManBubbles01.classList.add('active');
+            myStoryMan?.classList.add('active');
+          }, '+=.8')
+          .add(() => {
+            myStoryManBubbles01.classList.remove('active');
+            myStoryMan?.classList.remove('active');
+          }, '+=1.2')
+          .add(() => {
+            myStoryWomanBubbles01.classList.add('active');
+            myStoryWoman?.classList.add('active');
+          })
+          .add(() => {
+            myStoryWomanBubbles01.classList.remove('active');
+            myStoryWoman?.classList.remove('active');
+          }, '+=1.2');
+      });
+    })
+    .add(() => {
+      myStoryDragon?.addEventListener('animationend', () => {
+        console.log('uz si skoncil?');
+        gsapAfterDragon
+          .add(() => {
+            myStoryManBubbles02.classList.add('active');
+            myStoryMan?.classList.add('active');
+          }, '+=0.5')
+          .add(() => {
+            myStoryManBubbles02.classList.remove('active');
+            myStoryMan?.classList.remove('active');
+          }, '+=1.2')
+
+          .add(() => {
+            myStoryWomanBubbles02.classList.add('active');
+            myStoryMan?.classList.add('active');
+          })
+          .add(() => {
+            myStoryWomanBubbles02.classList.remove('active');
+            myStoryMan?.classList.remove('active');
+          }, '+=1.2');
+      });
+    });
 }
