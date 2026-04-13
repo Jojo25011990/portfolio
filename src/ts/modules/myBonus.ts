3804fbbea7eb7e0bb6560c5a8ac472136e4ab156
Jojo25011990 <Jojo25011990@gmail.com>
2026-04-13 12:38:14 +0200

    add all

--- src/ts/modules/myBonus.ts
+++ src/ts/modules/myBonus.ts
@@ -1,31 +1,48 @@
+import gsap from 'gsap';
+
 export default function myBonus() {
   const myBonusBook = document.querySelector<HTMLDivElement>('.my-bonus__book');
-
   const coverFront = document.querySelector<HTMLDivElement>(
     '.my-bonus__book-cover--front',
   );
   const coverBack = document.querySelector<HTMLDivElement>(
     '.my-bonus__book-cover--back',
   );
-
   const myPages = document.querySelectorAll<HTMLDivElement>(
     '.my-bonus__book-page--01,  .my-bonus__book-page--02, .my-bonus__book-page--03, .my-bonus__book-page--04, .my-bonus__book-page--05',
   );
-
   myPages.forEach(myPage =>
     myPage.addEventListener('click', () => {
-      myPage.classList.toggle('active');
+      if (!myPage.classList.contains('active')) {
+        myPage.classList.add('active');
+      } else {
+        myPage.classList.remove('active');
+      }
     }),
   );
-
   coverFront?.addEventListener('click', () => {
     coverFront.classList.toggle('active');
     myBonusBook?.classList.toggle('active');
   });
-
   coverBack?.addEventListener('click', () => {
-    coverBack.classList.toggle('active');
+    // coverBack.classList.toggle('active');
+    coverBack.classList.add('active');
     myBonusBook?.classList.toggle('active');
     myBonusBook?.classList.toggle('close');
+
+    setTimeout(() => {
+      coverBack.classList.remove('active');
+      [...myPages].reverse().forEach((page, index) => {
+        setTimeout(
+          () => {
+            page.classList.remove('active');
+          },
+          (index + 1) * 200,
+        );
+      });
+    }, 1000);
   });
+
+  // *** Version 02 ***
 }
+// *** End of Version 02 ***
