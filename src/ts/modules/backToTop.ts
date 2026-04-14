0889da5607cbafe9d1c7451e2e55d964e2038efb
Jojo25011990 <Jojo25011990@gmail.com>
2026-04-14 19:01:19 +0200

    add all

--- src/ts/modules/backToTop.ts
+++ src/ts/modules/backToTop.ts
@@ -16,5 +16,27 @@ export default function backToTop() {
     // *** Version 03 ***
     // condition ? backToTop?.classList.add('active') : null;
     // *** End of Version 03 ***
+
+    // *** Responsive Design ***
+    if (window.innerWidth <= 500 && backToTop)
+      backToTop.className = 'back-to-top';
+
+    // *** Version 02 ***
+    // backToTop.classList.remove('active');
+    // *** End of Version 02 ***
+
+    // *** Version 03 ***
+    // backToTop?.setAttribute('class', 'back-to-top');
+    // *** End of Version 03 ***
+    // *** End of Responsive Design ***
+
+    // *** Ninja Kun-fu style 🤣 ***
+    // if (backToTop) {
+    //   backToTop.classList.toggle(
+    //     'active',
+    //     condition && window.innerWidth > 500,
+    //   );
+    // }
+    // *** End of Ninja Kun-fu style 🤣 ***
   });
 }
