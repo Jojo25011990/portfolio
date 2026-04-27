0279743701da98f040ea8ada465f6f2bb6278381
Jojo25011990 <Jojo25011990@gmail.com>
2026-04-27 18:02:28 +0200

    add all

--- src/ts/modules/milestones.ts
+++ src/ts/modules/milestones.ts
@@ -4,23 +4,44 @@ import ScrollTrigger from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function milestones() {
+  const milestonesDescription = document.querySelector<HTMLParagraphElement>(
+    '.milestones__description',
+  );
+
+  const milestonesTitle =
+    document.querySelector<HTMLHeadingElement>('.milestones__title');
+  const milestonesTitleSpan = document.querySelector<HTMLSpanElement>(
+    '.milestones__title-span',
+  );
+
+  if (milestonesTitle) {
+    gsap.to(milestonesTitle, {
+      scrollTrigger: {
+        trigger: milestonesTitle,
+        start: 'top 70%',
+      },
+
+      onStart: () => {
+        milestonesTitleSpan?.classList.add('active');
+      },
+    });
+  }
+
+  if (milestonesDescription) {
+    gsap.to(milestonesDescription, {
+      scrollTrigger: {
+        trigger: milestonesDescription,
+        start: 'top center',
+      },
+
+      duration: 2.25,
+      text: 'A collection of key milestones that shaped how I approach building interfaces, animations and frontend systems.',
+      ease: 'none',
+    });
+  }
+
   const items = gsap.utils.toArray<HTMLElement>('.milestones__item');
 
-  //   let current = 0;
-  //   let target = 0;
-  //   let ease = 0.08;
-
-  //   function raf() {
-  //     target = window.scrollY;
-
-  //     current += (target - current) * ease;
-  //     items.forEach(item => {
-  //       item!.style.transform = `translateY(${-current}px)`;
-  //     });
-
-  //     requestAnimationFrame(raf);
-  //   }
-  //   raf();
   items.forEach((item, index) => {
     if (index < items.length - 1) {
       ScrollTrigger.create({
@@ -34,16 +55,31 @@ export default function milestones() {
       });
       ScrollTrigger.create({
         trigger: items[index + 1],
-        start: 'top bottom',
+        start: 'center bottom',
         end: 'top top',
         onUpdate: self => {
           const progressCard = self.progress;
           gsap.set(item, {
-            scale: 1 - progressCard * 0.15,
-            filter: `blur(${progressCard * 6}px)`,
+            scale: 1 - progressCard * 0.5,
           });
         },
       });
     }
   });
 }
+
+//   let current = 0;
+//   let target = 0;
+//   let ease = 0.08;
+
+//   function raf() {
+//     target = window.scrollY;
+
+//     current += (target - current) * ease;
+//     items.forEach(item => {
+//       item!.style.transform = `translateY(${-current}px)`;
+//     });
+
+//     requestAnimationFrame(raf);
+//   }
+//   raf();
