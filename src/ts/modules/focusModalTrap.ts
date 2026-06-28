f836144c5b49165330fd1634ae5bfcf13b6e1225
Jojo25011990 <Jojo25011990@gmail.com>
2026-06-28 16:19:09 +0200

    add all

---
+++ src/ts/modules/focusModalTrap.ts
@@ -0,0 +1,41 @@
+export default function focusModalTrap(
+  event: KeyboardEvent,
+  container: HTMLElement,
+) {
+  // *** Header | Features | Footer ***
+  if (event.key !== 'Tab') return;
+
+  const focusableElements = Array.from(
+    container?.querySelectorAll<HTMLElement>(
+      'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+    ),
+  );
+
+  if (!focusableElements.length) return;
+
+  const firstElement = focusableElements[0];
+  const lastElement = focusableElements[focusableElements.length - 1];
+
+  const currentIndexElement = focusableElements.indexOf(
+    document.activeElement as HTMLElement,
+  );
+
+  // *** Main Functionality ***
+  if (event.shiftKey) {
+    if (currentIndexElement <= 0) {
+      event.preventDefault();
+      lastElement.focus();
+    }
+  } else {
+    if (
+      currentIndexElement === focusableElements.length - 1 ||
+      currentIndexElement === -1
+    ) {
+      event.preventDefault();
+      firstElement.focus();
+    }
+  }
+  // *** End of Main Functionality ***
+
+  // *** End of Header | Features | Footer ***
+}
